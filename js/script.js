5240c840bd9dd9aa2ec801631fac959b14b300b6
ChriEffe <christianferrazzano93@gmail.com>
2021-12-02 17:20:27 +0100

    function per generare numeri random

--- js/script.js
+++ js/script.js
@@ -6,30 +6,19 @@ Il computer deve generare 16 numeri casuali nello stesso range della difficoltà
 I numeri nella lista delle bombe non possono essere duplicati.
 In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina, altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle. La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti. Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una b.*/
 
-// devo generare 16 numeri casuali senza farli duplicarre
-function getRndInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
-let numberRand = getRndInt(min, max);
-let bomb = [];
-while (numberRand) {
-    
-}
-// devo inserirli in ogni livello
-
-// clicco: se il numero è presente diventa rosso e finisce la partita, se non è presenta diventa blu e si continua; la partita finisce anche se finiscono i numeri consentiti
-
-// a partita terminata appare il numero di click effettuati = punteggio
-
-
+// seleziono pulsante play
 let play = document.querySelector('button');
+// eventi quando ci clicco sopra
 play.addEventListener('click', function () {
+    // selezione elementi dal dom
     let level = document.querySelector('select');
     let active = document.querySelector('.hidden');
     let grid = document.querySelector('.grid');
+    // dichiaro righe e colonne
     let row = 0;
     let col = 0;
-
+    
+    // definisco i livelli di difficoltà
     switch (level.value) {
         case 'easy':
             grid.innerHTML = '';
@@ -38,42 +27,75 @@ play.addEventListener('click', function () {
             row = 10;
             col = 10;
             createSquare(row, col);
+            bomb = getRndInt(1, 100);
             break;
-        case 'medium':
-            grid.innerHTML = '';
-            active.classList.remove('hidden');
-            active.classList.add('active');
-            row = 9;
-            col = 9;
-            createSquare(row, col);
-            break;
-        case 'hard':
-            grid.innerHTML = '';
-            active.classList.remove('hidden');
-            active.classList.add('active');
-            row = 7;
-            col = 7;
-            createSquare(row, col);
-            break;
+            case 'medium':
+                grid.innerHTML = '';
+                active.classList.remove('hidden');
+                active.classList.add('active');
+                row = 9;
+                col = 9;
+                createSquare(row, col);
+                bomb = getRndInt(1, 100);
+                break;
+                case 'hard':
+                    grid.innerHTML = '';
+                active.classList.remove('hidden');
+                active.classList.add('active');
+                row = 7;
+                col = 7;
+                createSquare(row, col);
+                bomb = getRndInt(1, 100);
+                break;
     }
 })
-
+        
+ // genero funzione per creare gli square
 function createSquare(row, col) {
+    // grandezza griglia in base agli square
     let numberSquare = row * col;
-
+    
+    // genero square
     for (let i = 0; i < numberSquare; i++) {
+        // seleziono griglia dal dom
         let grid = document.querySelector('.grid');
+        // creo div per inserire gli square
         let square = document.createElement('div');
+        // aggiungo classe e creo dimensioni square
         square.classList.add('square');
         square.style.width = `calc(${col})`;
         square.style.height = `calc(${row})`;
+        // inserisco square nella griglia
         let numbers = i + 1;
         square.append(numbers);
         grid.append('square');
-
+        
+        // quando clicco sullo square, diventa blu
         square.addEventListener('click', function () {
             this.style.backgroundColor = 'blue';
         })
-        
     }
 }
+
+// genero numeri random nell'array "bomb"
+function getRndInt(min, max) {
+    // creo array vuoto
+    let bomb = [];
+    
+    // inserisco numeri nell'array senza duplicati
+    for (let i = 0; i < 16; i++) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        
+        // min e max sono inclusivi
+        let element = bomb[i];
+        element = Math.floor(Math.random() * (max - min + 1)) + min;
+        
+        // controllo unicità numeri nell'array
+        while (bomb.includes(element)) {
+            element = Math.floor(Math.random() * (max - min + 1)) + min;
+        }
+        bomb.push(element);
+        }
+        return bomb
+    }
